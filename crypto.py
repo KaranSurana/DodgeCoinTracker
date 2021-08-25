346fec7036e8a063e635325c710fc3935fd4ddac
Karan Surana <70429983+KaranSurana@users.noreply.github.com>
2021-08-25 19:18:25 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import winsound
 from datetime import datetime
+import time
 
 frequency = 2500  # Set Frequency To 2500 Hertz
 duration = 200  # Set Duration To 1000 ms == 1 second
@@ -10,11 +11,9 @@ prev=0
 while True:
     html_text=requests.get('https://in.finance.yahoo.com/cryptocurrencies/').text
     soup=BeautifulSoup(html_text,'lxml')
-    job=soup.find('span',class_="Trsdu(0.3s)",attrs={"data-reactid": "252"}).text
-    if(str(prev)!=job):
-        winsound.Beep(frequency, duration)
-        now = datetime.now()
-
-        current_time = now.strftime("%H:%M:%S")
-        print("Dodge Coin Price Changed To",job,"at", current_time)
-    prev=job
+    job=soup.find('span',class_="Trsdu(0.3s)",attrs={"data-reactid": "284"}).text
+    winsound.Beep(frequency, duration)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Dodge Coin Price Is",job,"at", current_time)
+    time.sleep(15)
